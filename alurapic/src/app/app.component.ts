061bb3b89b743cad2955603360408b05c18ca37e
Weverton Reis <weverton.sant.reis@gmail.com>
2020-05-11 20:55:56 -0300

    1 - Bem começado, metade feito

---
+++ alurapic/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app weverton treinamento';
+  description = 'Leão';
+  url = 'https://img.ohmymag.com.br/article/480/animal/o-leao-e-conhecido-como-o-rei-dos-animais_362fc95c8f7188a27c2a0a6f70f15579948c2dfc.jpg';
+}
